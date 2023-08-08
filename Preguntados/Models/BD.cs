b90029c15b6a6eac579e84d02e81302e4ab52a29
JULIAN FURLAND OTERO <47392040@ort>
2023-08-08 10:43:03 -0300

    8/8

--- Preguntados/Models/BD.cs
+++ Preguntados/Models/BD.cs
@@ -3,7 +3,7 @@ using Dapper;
 
 public static class DB
 {
-    private static string ConnectionString { get; set; } = @"Server=A-PHZ2-CIDI-029;DataBase=Preguntados;Trusted_Connection=True;";
+    private static string ConnectionString { get; set; } = @"Server=localhost;DataBase=Preguntados;Trusted_Connection=True;";
     public static List<Categorias> ObtenerCategorias()
     {
         List<Categorias> ListaCategorias = new List<Categorias>();
@@ -24,6 +24,53 @@ public static class DB
         }
         return ListaDificultades;
     }
-    //public static Preguntas ObtenerPreguntas(int dificultad, int categoria)
-
+    public static List<Preguntas> ObtenerPreguntas(int dificultad, int categoria)
+    {
+        List<Preguntas> ListaPreguntas = new List<Preguntas>();
+        if (dificultad == -1 && categoria == -1)
+        {
+            using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Preguntas";
+            ListaPreguntas = db.Query<Preguntas>(sql).ToList();
+        }
+        }
+        if (dificultad != -1 && categoria == -1)
+        {
+             using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Preguntas WHERE iddificultad = @iddificultad";
+            ListaPreguntas = db.Query<Preguntas>(sql, new {iddificultad = dificultad}).ToList();
+        }
+        }
+        if (dificultad == -1 && categoria != -1)
+        {
+             using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Preguntas WHERE idcategoria = @idcategoria";
+            ListaPreguntas = db.Query<Preguntas>(sql, new {idcategoria = categoria}).ToList();
+        }
+        }
+        else
+        {
+             using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Preguntas WHERE iddificultad = @iddificultad AND idcategoria = @idcategoria";
+            ListaPreguntas = db.Query<Preguntas>(sql, new {iddificultad = dificultad}, new {idcategoria = categoria}).ToList();
+        }
+        }
+        return ListaPreguntas;
+    }
+    public static List<Respuestas> ObtenerRespuestas(List<Preguntas> ListaPreguntas)
+    {
+        List<Respuestas> ListaRespuestas = new List<Respuestas>();
+        foreach (Preguntas pregunta in ListaPreguntas)
+        {
+            using (SqlConnection db = new SqlConnection(ConnectionString))
+        {
+            string sql = "SELECT * FROM Respuestas WHERE idpregunta = @idpregunta";
+            ListaRespuestas.AddRange(db.Query<Respuestas>(sql, new {idpregunta = pregunta.idpregunta}).ToList());
+        }
+        }
+    }
 }
